a4e3f2ac4b0419c88aa76886f8b6678e46c19f55
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2020-04-05 17:23:01 +0200

    Kész.

---
+++ márc30/jaxb-example2/src/main/java/legoset/Main.java
@@ -0,0 +1,34 @@
+package legoset;
+
+import jaxb.JAXBHelper;
+
+import java.io.*;
+import java.time.Year;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        LegoSet legoSet = new LegoSet();
+        legoSet.setName("Imperial TIE Fighter");
+        legoSet.setTheme("Star Wars");
+        legoSet.setSubtheme("Solo");
+        legoSet.setNumber("75211");
+        legoSet.setYear(Year.parse("2018"));
+        legoSet.setPieces(519);
+        legoSet.setWeight(new Weight(0.89,"kg"));
+        legoSet.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
+        Set<String> tags = new HashSet<String>();
+        tags.add("Starfighter");
+        tags.add("Stormtrooper");
+        tags.add("Star Wars");
+        tags.add("Solo");
+        legoSet.setTags(tags);
+        List<Minifig> minifigs = new ArrayList<Minifig>();
+        minifigs.add(new Minifig(2,"Imperial Mudtrooper"));
+        minifigs.add(new Minifig(1,"Imperial Pilot"));
+        minifigs.add(new Minifig(1,"Mimban Stormtrooper"));
+        legoSet.setMinifigs(minifigs);
+
+        JAXBHelper.toXML(legoSet, System.out);
+    }
+}
